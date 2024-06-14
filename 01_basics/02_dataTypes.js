5df77928492319c44aae5d9542e9de683337a0b4
saurabhsushir <saurabhsushir127@gmail.com>
2024-06-14 10:50:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+// bigint
+string = ""
+// boolean => true/false
+// null => standalone value
+// undefined => we have not given values
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Saurabh");
+
+let age = 40;
+
+console.log(typeof age);
+
+// null is an object. so typeof null gives => oject
